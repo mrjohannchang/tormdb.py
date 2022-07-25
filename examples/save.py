02312e0f88ce2fba025d591e86ba439ad9d3baad
Johann Chang <mr.changyuheng@gmail.com>
2022-07-25 10:02:32 +0800

    Support saving the same object

--- examples/save.py
+++ examples/save.py
@@ -10,6 +10,26 @@ class Person:
     age: int
 
 
-p: Person = Person('Catherine', 24)
+@dataclasses.dataclass
+class Family:
+    wife: Person
+    husband: Person
 
-tormdb.save(p)
+
+wife: Person = Person('Catherine', 24)
+husband: Person = Person('Chris', 24)
+
+family: Family = Family(
+    wife=wife,
+    husband=husband)
+
+tormdb.save(family, 'save-example.db')
+
+
+wife: Person = Person('Amanda', 21)
+
+family2: Family = Family(
+    wife=wife,
+    husband=wife)
+
+tormdb.save(family2, 'save-example.db')
--- packages/tormdb/tormdb.py
+++ packages/tormdb/tormdb.py
@@ -1,42 +1,55 @@
-import inspect
-from typing import Any, Union
-import uuid
+from typing import Any, Dict, Union
 
 import dataset
+
+
+obj_to_stored_map: Dict[int, str] = dict()
+stored_to_obj_map: Dict[str, int] = dict()
+
+
+def get_ident(obj: Union[Any]) -> str:
+    if id(obj) not in obj_to_stored_map:
+        obj_to_stored_map[id(obj)] = str(id(obj))
+        stored_to_obj_map[str(id(obj))] = id(obj)
+    return obj_to_stored_map[id(obj)]
 
 
 def load(db: str):
     pass
 
 
-def save(db: Union[str, dataset.Database], obj: Any) -> str:
+def load_ident(obj: str):
+    pass
+
+
+def save(obj: Any, db: Union[str, dataset.Database]) -> str:
+    print(f'{db=}, {obj=}')
+
     if not isinstance(db, dataset.Database):
         db_ins: dataset.Database
         with dataset.connect(f'sqlite:///{db}?check_same_thread=False') as db_ins:
             db_ins.executable.execute('PRAGMA journal_mode=WAL')
-            return save(db_ins, obj)
+            return save(obj, db_ins)
 
-    ident: str = str(uuid.uuid4())[:8]
     attr: str
     val: Any
-    for attr, val in vars(obj):
-
+    for attr, val in vars(obj).items():
         if attr.startswith('_'):
-            print(f'{attr=} starts with _')
+            print(f'{attr} starts with _')
             continue
 
-        if callable(val) and not inspect.isclass(val):
-            print(f'{attr=} is a callable')
+        if callable(val):
+            print(f'{attr} is a callable')
             continue
 
-        if not inspect.isclass(val):
-            print(f'{attr=} is a {type(val)}')
-            db[f'{obj.__name__}#{ident}'].insert(
-                dict(name=attr, type=type(val), value=val))
+        if val.__class__.__module__ == 'builtins':
+            print(f'{attr} is an instance of the built-in class {type(val).__name__}')
+            db[f'{type(obj).__name__}#{get_ident(obj)}'].upsert(
+                dict(name=attr, type=type(val).__name__, value=val), ['name', 'value'])
+            continue
 
-        print(f'{attr=} is {val.__name__}')
-        type_name: str = save(db, val)
-        db[f'{obj.__name__}#{ident}'].insert(
-            dict(name=attr, type=type_name, value=save(db, val)))
+        print(f'{attr} is an instance of {type(val).__name__}')
+        db[f'{type(obj).__name__}#{get_ident(obj)}'].upsert(
+            dict(name=attr, type=type(val).__name__, value=save(val, db)), ['name', 'value'])
 
-    return f'{obj.__name__}#{ident}'
+    return f'{type(obj).__name__}#{get_ident(obj)}'
